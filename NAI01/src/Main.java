611b05464222bece52c6287f90ecf76daa5a473b
s31552 <s31552@pjwstk.edu.pl>
2025-03-09 19:28:37 +0100

    Main.java: Sort, limit map and extract the object with the most count; modify .gitignore and .data files

--- NAI01/src/Main.java
+++ NAI01/src/Main.java
@@ -1,22 +1,21 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         int k = 5; // normalnie Integer.parseInt(args[0])
-        Map<String, Double> distances = new HashMap<>();
-        String testRecord = "5.0,3.2,1.2,0.2,Iris-setosa";
+        Map<String, Double> distances = new LinkedHashMap<>();
+        String test_record = "5.0,3.2,1.2,0.2,Iris-setosa";
+        String[] test_record_split = test_record.split(",");
+        String test_attribute = test_record_split[test_record_split.length - 1];
         List<Double> testRecordDimensions = new ArrayList<>();
         int dimensionsNumber;
 
         for (int i = 0; true; i++) {
             try {
-                testRecordDimensions.add(Double.parseDouble(testRecord.split(",")[i]));
+                testRecordDimensions.add(Double.parseDouble(test_record.split(",")[i]));
             } catch (Exception e) {
                 dimensionsNumber = i;
                 break;
@@ -47,5 +46,23 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        distances = distances.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .limit(k)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new
+                ));
+
+        Map<String, Integer> counts = new HashMap<>();
+
+        for (Map.Entry<String, Double> entry : distances.entrySet()) {
+            if (counts.containsKey(entry.getKey()))
+                counts.put(entry.getKey(), counts.get(entry.getKey()) + 1);
+            else
+                counts.put(entry.getKey(), 1);
+        }
+
+        String[] train_record_mostCount = Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey().split(",");
+        String train_attribute_mostCount = train_record_mostCount[train_record_mostCount.length - 1];
     }
 }
