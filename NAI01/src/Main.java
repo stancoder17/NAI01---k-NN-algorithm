48d6c04d1e4462a70d4b46ce14eb668b45e4d09b
s31552 <s31552@pjwstk.edu.pl>
2025-03-09 19:50:55 +0100

    Add implementation for all test records in iris.test.data file and calculate accuracy

--- NAI01/src/Main.java
+++ NAI01/src/Main.java
@@ -1,68 +1,98 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        int k = 5; // normalnie Integer.parseInt(args[0])
-        Map<String, Double> distances = new LinkedHashMap<>();
-        String test_record = "5.0,3.2,1.2,0.2,Iris-setosa";
-        String[] test_record_split = test_record.split(",");
-        String test_attribute = test_record_split[test_record_split.length - 1];
-        List<Double> testRecordDimensions = new ArrayList<>();
-        int dimensionsNumber;
+        int k = 35; // normalnie Integer.parseInt(args[0])
+        int correctCount = 0;
+        int incorrectCount = 0;
 
-        for (int i = 0; true; i++) {
-            try {
-                testRecordDimensions.add(Double.parseDouble(test_record.split(",")[i]));
-            } catch (Exception e) {
-                dimensionsNumber = i;
-                break;
-            }
-        }
-
-        File file = new File("./iris.train.data");
+        File testFile = new File("./iris.test.data");
         try {
-            BufferedReader in = new BufferedReader(new FileReader(file));
-            String line;
-            while ((line = in.readLine()) != null) {
-                String[] lineAllElements = line.split(",");
-                List<Double> lineDimensions = new ArrayList<>();
+            BufferedReader in_test = new BufferedReader(new FileReader(testFile));
+            String test_record;
+            while ((test_record = in_test.readLine()) != null) {
+                String[] test_record_split = test_record.split(",");
+                String test_attribute = test_record_split[test_record_split.length - 1];
 
-                for (int i = 0; i < dimensionsNumber; i++)
-                    lineDimensions.add(Double.parseDouble(lineAllElements[i]));
+                Map<String, Double> distances = new LinkedHashMap<>();
+                List<Double> testRecordDimensions = new ArrayList<>();
+                int dimensionsNumber;
 
-                double sum = 0.0;
+                for (int i = 0; true; i++) {
+                    try {
+                        testRecordDimensions.add(Double.parseDouble(test_record.split(",")[i]));
+                    } catch (Exception e) {
+                        dimensionsNumber = i;
+                        break;
+                    }
+                }
 
-                // Getting sum of (x1 - x2)^2
-                for (int i = 0; i < dimensionsNumber; i++)
-                    sum += Math.pow(testRecordDimensions.get(i) - lineDimensions.get(i), 2);
+                File trainFile = new File("./iris.train.data");
+                try {
+                    BufferedReader in_train = new BufferedReader(new FileReader(trainFile));
+                    String line_train;
+                    while ((line_train = in_train.readLine()) != null) {
+                        String[] lineAllElements = line_train.split(",");
+                        List<Double> lineDimensions = new ArrayList<>();
 
-                Double result = Math.sqrt(sum);
+                        for (int i = 0; i < dimensionsNumber; i++)
+                            lineDimensions.add(Double.parseDouble(lineAllElements[i]));
 
-                distances.put(line, result);
+                        double sum = 0.0;
+
+                        // Getting sum of (x1 - x2)^2
+                        for (int i = 0; i < dimensionsNumber; i++)
+                            sum += Math.pow(testRecordDimensions.get(i) - lineDimensions.get(i), 2);
+
+                        Double result = Math.sqrt(sum);
+
+                        distances.put(line_train, result);
+                    }
+
+                    in_train.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                distances = distances.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue())
+                        .limit(k)
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new
+                        ));
+
+                Map<String, Integer> counts = new HashMap<>();
+
+                for (Map.Entry<String, Double> entry : distances.entrySet()) {
+                    if (counts.containsKey(entry.getKey()))
+                        counts.put(entry.getKey(), counts.get(entry.getKey()) + 1);
+                    else
+                        counts.put(entry.getKey(), 1);
+                }
+
+                String[] train_record_mostCount = Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey().split(",");
+                String train_attribute_mostCount = train_record_mostCount[train_record_mostCount.length - 1];
+
+                if (train_attribute_mostCount.equals(test_attribute))
+                    correctCount++;
+                else
+                    incorrectCount++;
+
             }
+
+            in_test.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        distances = distances.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .limit(k)
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new
-                ));
 
-        Map<String, Integer> counts = new HashMap<>();
+        double accuracy = 100.0 - ((double) incorrectCount / (correctCount + incorrectCount));
+        System.out.println(correctCount);
+        System.out.println(incorrectCount);
+        System.out.println(accuracy + "%");
 
-        for (Map.Entry<String, Double> entry : distances.entrySet()) {
-            if (counts.containsKey(entry.getKey()))
-                counts.put(entry.getKey(), counts.get(entry.getKey()) + 1);
-            else
-                counts.put(entry.getKey(), 1);
-        }
-
-        String[] train_record_mostCount = Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey().split(",");
-        String train_attribute_mostCount = train_record_mostCount[train_record_mostCount.length - 1];
     }
 }
